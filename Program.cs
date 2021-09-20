c4f19967d1d24cf7f9a5f723bf5ac2859b5fda08
Murad Ragimov <ragimov73ya@mail.ru>
2021-09-20 13:54:53 +0300

    Исправил код.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ПР2
+{
+    class Program
+    {
+        static void Main()
+
+        {
+            double x = 3.14, y = 17.25;
+
+            double add, sub, mul, div;
+
+            add = x + y;
+
+            sub = x - y;
+
+            mul = x * y;
+
+            div = x / y;
+
+            Console.Write("add = " + add + "\n\n" +
+                     "sub = " + sub + "\n\n" +
+                     "mul = " + mul + "\n\n" +
+                     "div = " + div);
+            Console.ReadLine();
+        }
+    }
+}
